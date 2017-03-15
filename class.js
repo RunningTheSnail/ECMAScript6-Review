33a68c36b86eb9e5d15039f45f8e1e0ef714ffaf
谭帅 <jerry@danwi.me>
2017-03-15 13:58:57 +0800

    Function拥有__proto__(指向父类构造函数)和prototype属性(prototype.__proto__指向父类的原型对象),对象只具有__proto__属性(指向该类的原型对象)

--- class.js
+++ class.js
@@ -6,9 +6,22 @@ class Foo {
 
 }
 
+class Bar extends Foo {
+
+}
+
 new Foo();
 
+//Function 拥有prototype和__proto__属性
+
+//表示方法的继承
+//Function.prototype.__proto__指向父类的prototype
+
+//表示构造函数的继承
+//Function.__proto__指向父类
 console.log(Foo.prototype.__proto__ === Object.prototype);
+
+console.log(Object.prototype.__proto__);
 
 var foo = new Foo();
 
@@ -16,3 +29,10 @@ console.log(foo.__proto__ === Foo.prototype);
 
 //表示没有这个属性 undefined
 console.log(foo.prototype);
+
+var bar = new Bar();
+
+//对象只有__proto__属性  指向父类的原型对象
+console.log(bar.__proto__ === Bar.prototype);
+
+console.log(Bar.__proto__ == Foo);
