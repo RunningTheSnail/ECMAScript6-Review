79bf23aeb2ec8fe1d647d2ec493fd6cefd0a6961
谭帅 <jerry@danwi.me>
2017-03-15 13:41:47 +0800

    __proto__  和prototype 区别

--- class.js
+++ class.js
@@ -7,3 +7,12 @@ class Foo {
 }
 
 new Foo();
+
+console.log(Foo.prototype.__proto__ === Object.prototype);
+
+var foo = new Foo();
+
+console.log(foo.__proto__ === Foo.prototype);
+
+//表示没有这个属性 undefined
+console.log(foo.prototype);
