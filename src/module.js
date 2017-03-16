de93ad2f8c11b90c0617a4590a6588eb65504edb
谭帅 <jerry@danwi.me>
2017-03-16 10:21:58 +0800

    export 不能在块级作用域中使用

--- src/module.js
+++ src/module.js
@@ -1,5 +1,20 @@
 /**
  * Created by toonew on 2017/3/15.
  */
+
+//接口
 export var m = 1;
+
+export var get = function () {
+
+};
+
+class A {
+  set() {
+    console.log('set');
+  }
+}
+
+export {A}
+
 
--- src/module_test.js
+++ src/module_test.js
@@ -1,6 +1,10 @@
 /**
  * Created by toonew on 2017/3/15.
  */
-import {m} from './module'
+import {m, A} from './module'
 
 console.log(m);
+
+var a = new A();
+
+a.set();
